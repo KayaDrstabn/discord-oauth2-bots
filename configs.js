ba3a4f8209f4bc6a7cd43006e6a2dfbf3ab7ce7c
KayaDrstabn <108328937+KayaDrstabn@users.noreply.github.com>
2024-07-03 12:21:20 +0400

    Add files via upload

---
+++ configs.js
@@ -0,0 +1,14 @@
+module.exports = {
+  token: process.env.token, //Bot Token
+  port: "80",
+  prefix: ".", //Bot Prefix
+  client_id: "1239269903677001848", //Client ID
+  client_secret: "0qf9BBy0RkiFFokfzplmsnVEaIOtG6OR", //Client Secret
+  redirect_uri: "https://a15f73ec-3f0f-4b60-be8a-af0a3c3c839e-00-2opsn2uwo423l.spock.replit.dev/login", //Changing the Port Part!
+  idserver: "1214280471496892516", //Server ID Main
+  idrole: "1214280471496892516", //Role ID Main
+  webhook: "https://discord.com/api/webhooks/1239252551153750066/QT1u_FeIaUxuSsEw1N8yascnqsZhEEszSeuMzuMMRbUzW64LBsgDqVSc9vneWJ1jLkuo", //Auth Log
+  webhookBackup: "https://discord.com/api/webhooks/1239252551153750066/QT1u_FeIaUxuSsEw1N8yascnqsZhEEszSeuMzuMMRbUzW64LBsgDqVSc9vneWJ1jLkuo", //wehbook de backup do object.json
+  authLink: `https://discord.com/oauth2/authorize?client_id=1239269903677001848&response_type=code&redirect_uri=https%3A%2F%2Fa15f73ec-3f0f-4b60-be8a-af0a3c3c839e-00-2opsn2uwo423l.spock.replit.dev%2Flogin&scope=identify+guilds.join`, //Auth Link
+  owners: ["1214250364597960775", "", ""], //Owner ID
+};
